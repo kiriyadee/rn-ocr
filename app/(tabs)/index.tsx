4195a8b6814c3fd193e9752b4237e4c437b88bd0
WIT <wit.chanthawat@gmail.com>
2024-11-24 01:07:02 +0700

    .

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,23 +1,10 @@
 import { Text, View } from "@/components/Themed";
 import React from "react";
 import { StyleSheet } from "react-native";
-import {
-  useCameraDevice,
-  useCameraPermission,
-} from "react-native-vision-camera";
 
 import EditScreenInfo from "@/components/EditScreenInfo";
-import NoCameraDeviceError from "@/components/NoCameraDeviceError";
-
-import PermissionsPage from "@/components/PermissionPage";
-import { Camera } from "react-native-vision-camera";
 
 export default function TabOneScreen() {
-  const device = useCameraDevice("back");
-  const { hasPermission } = useCameraPermission();
-
-  if (!hasPermission) return <PermissionsPage />;
-  if (device == null) return <NoCameraDeviceError />;
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Tab One</Text>
@@ -27,7 +14,6 @@ export default function TabOneScreen() {
         darkColor="rgba(255,255,255,0.1)"
       />
       <EditScreenInfo path="app/(tabs)/index.tsx" />
-      <CameraView />
     </View>
   );
 }
@@ -48,13 +34,3 @@ const styles = StyleSheet.create({
     width: "80%",
   },
 });
-
-const CameraView = () => {
-  const device = useCameraDevice("back");
-
-  if (device == null) return <NoCameraDeviceError />;
-
-  return (
-    <Camera style={StyleSheet.absoluteFill} device={device} isActive={true} />
-  );
-};
